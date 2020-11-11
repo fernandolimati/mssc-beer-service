67db9c49c62f3f762852ab6183a0cfa555eefbaa
Fernando Lima <fernando.lima@siga-med.com>
2020-11-10 23:23:46 -0300

    Adding web data model. closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,9 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * Created by Fernando Lima
+ * at 10/11/2020
+ **/
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON;
+}
